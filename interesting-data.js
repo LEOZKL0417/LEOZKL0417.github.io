d8a218153b60b7454c355f0b168c6581e2e3e6cf
Kunlong Zhang <kunlong@umich.edu>
2026-03-09 19:59:01 -0400

    feat: add personal homepage

--- interesting-data.js
+++ interesting-data.js
@@ -7,7 +7,7 @@ window.interestingItems = [
   title: "Memo",
   source: "github",
   sourceUrl: "https://github.com/LEOZKL0417/MEMO.git",
-  summary: "Memo APP的Update",
-  thoughts: "这里写你的想法 thoughts"
+  summary: "已合并 recommend system 与 chatbot 功能，并新增 chatbot 的「猜你想问」feature。",
+  thoughts: "今天完成了 recommend system 和 chatbot 的功能合并；chatbot 已添加「猜你想问」功能；当前进度是等待登录功能接入。"
 }
 ];
